c0ae64254121de17b0f26b27af773de711f0ad05
AkshayADT <akshay.dangi@engineermaster.in>
2023-11-20 11:31:19 +0530

    routes and csrf verfy for post method route in any

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PhonePeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('phonepe',[PhonePeController::class,'phonepe']);
+
+Route::any('phonepe-response',[PhonePeController::class,'response'])->name('phonepe-response');
+
